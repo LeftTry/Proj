68aa88bfe8a57aa9a89984f9f3204083c07499d0
LeftTry <lerest.go@gmail.com>
2021-09-11 10:00:00 +0300

    Revert "Changed to russian, added question about party"
    This reverts commit a4ec04c54f276b3f90d89f2a7daae663e3332227.

--- main.cpp
+++ main.cpp
@@ -32,10 +32,10 @@ using namespace std;
 int main(){
     //ios_base::sync_with_stdio(false);
     //cin.tie(NULL);
-    str sur, name, fav, pty;
-    cout << "Имя " << "Фамилия " << "Любимая книга " << "Партия?";
-    cin >> name >> sur >> fav >> pty;
+    str sur, name, fav;
+    cout << "name " << "surname " << "favorite book";
+    cin >> name >> sur >> fav;
     ofstream file("output.txt");
-    file << name << endl << sur << endl << fav << endl << pty;
+    file << name << endl << sur << endl << fav;
     return 0;
 }
